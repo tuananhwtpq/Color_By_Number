ff79409433719b0828ee51b78b369fea55cae485
anhttpova <anhtt@dktechgroup.com>
2026-08-17 18:00:22 +0700

    update success screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,6 +80,7 @@ android {
 
 dependencies {
 
+    implementation(libs.androidx.activity.ktx)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
---
+++ app/src/main/java/com/example/baseproject/activities/PictureCompletedActivity.kt
@@ -0,0 +1,26 @@
+package com.example.baseproject.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.baseproject.R
+import com.example.baseproject.bases.BaseActivity
+import com.example.baseproject.databinding.ActivityPictureCompletedBinding
+
+class PictureCompletedActivity : BaseActivity<ActivityPictureCompletedBinding>(
+    ActivityPictureCompletedBinding::inflate
+) {
+
+    override fun initData() {
+
+    }
+
+    override fun initView() {
+    }
+
+    override fun initActionView() {
+    }
+
+}
